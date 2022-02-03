9db9da97c10a74955f87f0597bb98c414df914d4
Masood Rezazadeh <rezazadehmasood@gmail.com>
2022-02-03 21:03:15 +0330

    Updates the persian word regex duo to not working on some android phones

--- components/Board.tsx
+++ components/Board.tsx
@@ -65,7 +65,8 @@ const Board: FC<props> = ({}) => {
   );
 
   const isValidInput = (input: string) => {
-    const persianLetters = /^[\u0600-\u06FF]$/;
+    const persianLetters =
+      /^[\u0621-\u0628\u062A-\u063A\u0641-\u0642\u0644-\u0648\u064E-\u0651\u0655\u067E\u0686\u0698\u06A9\u06AF\u06BE\u06CC]$/;
     return input.match(persianLetters);
   };
 
