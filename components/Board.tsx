cf38bed16521397795df8f59b47e9f67e9d5d822
Masood Rezazadeh <rezazadehmasood@gmail.com>
2022-02-03 21:30:30 +0330

    Update persian letters regex

--- components/Board.tsx
+++ components/Board.tsx
@@ -66,7 +66,7 @@ const Board: FC<props> = ({}) => {
 
   const isValidInput = (input: string) => {
     const persianLetters =
-      /^[\u0621-\u0628\u062A-\u063A\u0641-\u0642\u0644-\u0648\u064E-\u0651\u0655\u067E\u0686\u0698\u06A9\u06AF\u06BE\u06CC]$/;
+      /^[\u0622\u0627\u0628\u067E\u062A-\u062C\u0686\u062D-\u0632\u0698\u0633-\u063A\u0641\u0642\u06A9\u06AF\u0644-\u0648\u06CC]$/;
     return input.match(persianLetters);
   };
 
